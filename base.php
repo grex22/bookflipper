6a165e20ac93f13f58099fde89143a319ba5a549
Greg Burkett <greg@ripfish.com>
2015-10-29 18:29:56 -0400

    Initial Commit

--- base.php
+++ base.php
@@ -16,6 +16,43 @@ use Roots\Sage\Wrapper;
     <![endif]-->
     <?php
       do_action('get_header');
+    ?>
+    <?php
+      if(is_front_page()):
+      get_template_part('templates/home_header');
+    ?>
+      <section class="home_section masthead">
+        <div class="row">
+          <div class="col-xs-8 col-xs-offset-2 text-center">
+            <h2>IT TAKES TIME TO DRIVE AROUND AND SOURCE BOOKS PHYSICALLY.</h2>
+            <h1>Welcome to the Book Sourcing Solution for FBA Sellers.</h1>
+          </div>
+        </div>
+      </section>
+      <section class="home_section">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-6 col-xs-offset-3 text-center">
+              <a href="#" class="btn btn-primary">Login</a>
+              <a href="#" class="btn btn-primary btn-lg">Sign Up <em>Now.</em></a>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius libero dolor, at ornare diam commodo vitae. Nam eget urna sapien. Sed dignissim placerat purus nec sodales. Ut eu orci vitae purus gravida varius.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="home_section alt">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-6 col-xs-offset-3 text-center">
+              <a href="#" class="btn btn-primary">Login</a>
+              <a href="#" class="btn btn-primary btn-lg">Sign Up <em>Now.</em></a>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius libero dolor, at ornare diam commodo vitae. Nam eget urna sapien. Sed dignissim placerat purus nec sodales. Ut eu orci vitae purus gravida varius.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    <?php 
+      else: // else is NOT home page
       get_template_part('templates/header');
     ?>
     <div class="wrap container" role="document">
@@ -31,6 +68,8 @@ use Roots\Sage\Wrapper;
       </div><!-- /.content -->
     </div><!-- /.wrap -->
     <?php
+      endif; //endif if isn't home page
+      
       do_action('get_footer');
       get_template_part('templates/footer');
       wp_footer();
--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,13 @@
 <footer class="content-info">
   <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+    <div class="text-center footer_social">
+      <a href="http://facebook.com"><img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/facebook.png"></a>
+      <a href="http://twitter.com"><img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/twitter.png"></a>
+    </div>
+    <div class="footer_wrap">
+      <div class="footer_box">
+      &copy;<?php echo date("Y");?> The BookFlipper
+      </div>
+    </div>
   </div>
 </footer>
